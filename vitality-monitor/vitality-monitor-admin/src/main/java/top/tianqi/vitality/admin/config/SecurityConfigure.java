76afb5377cb6f1635fab00072891accb040bbc4d
tianqi <823507949@qq.com>
2020-08-19 22:19:46 +0800

    添加监控模块

--- vitality-monitor/vitality-monitor-admin/src/main/java/top/tianqi/vitality/admin/config/SecurityConfigure.java
+++ vitality-monitor/vitality-monitor-admin/src/main/java/top/tianqi/vitality/admin/config/SecurityConfigure.java
@@ -1,7 +1,10 @@
 package top.tianqi.vitality.admin.config;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
  * Security配置类
@@ -11,6 +14,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @EnableWebSecurity
 public class SecurityConfigure extends WebSecurityConfigurerAdapter {
+    private final String adminContextPath;
 
+    public SecurityConfigure(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
 
 }
